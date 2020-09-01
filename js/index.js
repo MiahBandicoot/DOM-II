61b81cfeb6d5a4b50350968d5bdbd5f7c3ec16ae
Miah <revolution5117@gmail.com>
2020-09-01 07:59:30 -0700

    working on project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const name = document.querySelector(".main-navigation h1")
+name.addEventListener("mouseover", function(event){
+    event.target.style.color = "red";
+
+    setTimeout(function(){
+event.target.style.color = "";
+    },300)
+},false);
+
+const navi = document.querySelector(".main-navigation nav")
+navi.addEventListener("mouseover", function(event){
+    event.target.style.color = "orange";
+
+    setTimeout(function(){
+        event.target.style.color = "";
+            },300)
+        },false);
